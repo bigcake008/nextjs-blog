a8146cf5ced7e0df321e0f4a99e9b34606c4ca85
sujane <sujane@163.com>
2021-08-02 17:03:45 +0800

    Image component test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,7 @@ import Date from "../../components/date";
 import {getAllPostIds, getPostData} from "../../lib/posts";
 import utilStyles from '../../styles/utils.module.css';
 
+
 export default function Post({postData}) {
     return (
         <Layout>
@@ -14,9 +15,9 @@ export default function Post({postData}) {
             <article>
                 <h1 className={utilStyles.headingXl}>{postData.title}</h1>
                 <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
+                    <Date dateString={postData.date}/>
                 </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
             </article>
         </Layout>
     )
@@ -30,11 +31,11 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({params}) {
-    const postData = await getPostData(params.id);
+export async function getStaticProps(context) {
+    const postData = await getPostData(context.params.id);
     return {
         props: {
-            postData
+            postData: postData
         }
     }
 }
