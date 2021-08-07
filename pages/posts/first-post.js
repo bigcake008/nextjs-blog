8e6ad30489da0b2a2a02d41bad6f7c1e4e440424
sujane <sujane@163.com>
2021-08-07 14:51:09 +0800

    testing

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import Image from "next/image";
 
 export default function FirstPost() {
     return (
@@ -9,8 +10,10 @@ export default function FirstPost() {
                 <title>First Post</title>
             </Head>
             <h1>First Post</h1>
-            <img src={'/images/whistle.gif'}
-                 alt='whistle!'
+            <Image src='/images/whistle.gif'
+                   alt='blow whistle'
+                   width={272}
+                   height={480}
             />
             
         </Layout>
